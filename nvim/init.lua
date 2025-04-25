43a284ea764a83d92567afe982fb922c383b5404
VSparl <sparleanu.victor@gmail.com>
2025-04-25 15:56:55 +0200

    Fix copy-pasting and configure tabs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,12 @@
---- make nvim use the system clipboard
+--- use the system clipboard
 vim.opt.clipboard = 'unnamedplus'
+--- don't copy stuff when deleting
+vim.keymap.set({'n', 'v'}, 'd', '"_d', { noremap = true })
+vim.keymap.set('n', 'dd', '"_dd', { noremap = true })
+vim.keymap.set({'n', 'v'}, 'x', '"_x', { noremap = true })
+--- set tab  to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
