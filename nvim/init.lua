86eec3f7551e03c0c488cd6851902f15ba566a54
VSparl <sparleanu.victor@gmail.com>
2025-05-28 11:06:51 +0200

    replaced tildes with line numbers

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,15 @@
 --- use the system clipboard
 vim.opt.clipboard = 'unnamedplus'
+--- Remove the tilde in empty space
+vim.opt.fillchars:append({ eob = " " })
+--- Enable absolute line numbers
+vim.opt.number = true
+
 --- don't copy stuff when deleting
 vim.keymap.set({'n', 'v'}, 'd', '"_d', { noremap = true })
 vim.keymap.set('n', 'dd', '"_dd', { noremap = true })
 vim.keymap.set({'n', 'v'}, 'x', '"_x', { noremap = true })
+
 --- set tab  to 4 spaces
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
