f3fc9eed4a9e5d088d6fc8f67af462dd3ab79e28
VSparl <sparleanu.victor@gmail.com>
2025-09-14 15:52:46 +0200

    moved keybinds to separate file

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,10 +7,6 @@ vim.opt.number = true
 -- Disable word wrap
 vim.opt.wrap = false
 
--- don't copy stuff when deleting
-vim.keymap.set({'n', 'v'}, 'd', '"_d', { noremap = true })
-vim.keymap.set('n', 'dd', '"_dd', { noremap = true })
-
 -- set tab  to 4 spaces
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -18,22 +14,12 @@ vim.opt.smartindent = true
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
--- remove highlighting when pressing escape
-vim.keymap.set({'n'}, '<ESC>', '<CMD>nohls<CR>', { noremap = true })
-
 -- set leader keys
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>')
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set({'n'}, '<leader>d', '<CMD>lua vim.diagnostic.open_float()<CR>', { desc = 'Show diagnostics under the cursor' })
-
--- Map Ctrl+V to paste in insert mode
-vim.keymap.set('i', '<C-v>', '<C-r>+', { noremap = true, silent = true })
-
--- Add `:Whitespace` as command to trim trailing whitespace
-vim.api.nvim_create_user_command('Whitespace', '%s/\\s\\+$//g', {})
-
--- Load terminal config and plugins
+-- Load configs defined in other files
+require("config.keybinds")
 require("config.terminal")
 require("config.lazy")
---
+++ nvim/lua/config/keybinds.lua
@@ -0,0 +1,18 @@
+-- don't copy stuff when deleting
+vim.keymap.set({'n', 'v'}, 'd', '"_d', { noremap = true })
+vim.keymap.set('n', 'dd', '"_dd', { noremap = true })
+
+-- remove highlighting when pressing escape
+vim.keymap.set({'n'}, '<ESC>', '<CMD>nohls<CR>', { noremap = true })
+
+-- Map Ctrl+V to paste in insert mode
+vim.keymap.set('i', '<C-v>', '<C-r>+', { noremap = true, silent = true })
+
+-- Diagnostics
+vim.keymap.set({'n'}, '<LEADER>d', '<CMD>lua vim.diagnostic.open_float()<CR>', { desc = 'Show diagnostics under the cursor' })
+
+-- Open file explorer. Further keymaps are in the config file of the explorer plugin.
+vim.keymap.set({'n'}, '<LEADER>eo', '<CMD>:Oil<CR>', { desc = 'Open file explorer here' })
+
+-- Select everything in document
+vim.keymap.set({'n', 'i', 'v'}, '<C-a>', '<ESC>ggVG', { desc = 'Select entire buffer' })
