67836c1c9e0844c9fa84ec1495e1b8377872b0cd
VSparl <sparleanu.victor@gmail.com>
2025-10-26 20:16:11 +0100

    Added VSCode theme option

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,18 +1,28 @@
 return {
+    -- ## Everforest theme ##
+    -- {
+    --     "sainnhe/everforest",
+    --     lazy = false,
+    --     priority=1000,
+    --     config = function()
+    --         vim.g.everforest_enable_italic = 1
+    --         vim.g.everforest_diagnostic_text_highlight = 1  -- Highlight errors, warnings, etc.
+    --         vim.g.everforest_disable_terminal_colors = 1  -- Keep same terminal colours as outside nvim
+    --         vim.g.everforest_background = "hard"  -- Stronger contrast
+    --         -- vim.g.everforest_transparent_background = 1
+    --         vim.cmd([[colorscheme everforest]])
+    --     end,
+    -- },
+    --
+    --
+    -- { "nvim-tree/nvim-web-devicons", lazy = true },
+
+    -- ## Visual Studio Code theme ##
     {
-        "sainnhe/everforest",
-        lazy = false,
-        priority=1000,
-        config = function()
-            vim.g.everforest_enable_italic = 1
-            vim.g.everforest_diagnostic_text_highlight = 1  -- Highlight errors, warnings, etc.
-            vim.g.everforest_disable_terminal_colors = 1  -- Keep same terminal colours as outside nvim
-            vim.g.everforest_background = "hard"  -- Stronger contrast
-            -- vim.g.everforest_transparent_background = 1
-            vim.cmd([[colorscheme everforest]])
-        end,
-    },
-
-
-    { "nvim-tree/nvim-web-devicons", lazy = true },
+      "gamunu/vscode.nvim",
+      priority = 100,
+      config = function()
+        vim.cmd([[colorscheme vscode]])
+      end,
+    }
 }
